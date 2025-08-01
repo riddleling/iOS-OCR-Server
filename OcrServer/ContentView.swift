e9edc6fcf0f8844b1413a2e3e00aa8e55b556639
Riddle Ling <riddle.apple@gmail.com>
2025-08-01 20:57:29 +0800

    Initial Commit

---
+++ OcrServer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OcrServer
+//
+//  Created by Riddle Ling on 2025/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OcrServer/OcrServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OcrServerApp.swift
+//  OcrServer
+//
+//  Created by Riddle Ling on 2025/8/1.
+//
+
+import SwiftUI
+
+@main
+struct OcrServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
