0d1fd15ed805ca508ef7057dd38a91e610274ce2
Riddle Ling <riddle.apple@gmail.com>
2025-08-10 08:50:27 +0800

    fixed icon

--- OcrServer/Monitor/DashboardView.swift
+++ OcrServer/Monitor/DashboardView.swift
@@ -36,7 +36,7 @@ struct DashboardView: View {
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button(action: { sampler.clear() }) {
-                        Image(systemName: "trash")
+                        Image(systemName: "arrow.clockwise")
                     }
                 }
                 ToolbarItem(placement: .navigationBarLeading) {
